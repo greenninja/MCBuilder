2aaafca0a1c2e2da53cadbfd2b5e560d8f0a5ec8
Audra Brown <abrown360@gatech.edu>
2016-07-06 08:18:44 -0600

    absKeep

--- MCBuilder.py
+++ MCBuilder.py
@@ -24,16 +24,39 @@ def keep1(ir, ht, gl, block, block_sub, floor, ceiling, filename):
     else:
         f.write(s+'~'+str(ir)+' '+str(gl)+' ~'+str(ir)+' ~'+str(ir/-1)+' '+str(ht+gl-1)+' ~'+str(ir/-1)+' '+'air'+'\n')    
 
+    return True
 
-#    for c in cmds:
-#        final += crCommand(c,i)
-#        i += 1
+#build a single-walled, square keep (absolute coordinates)
+#clears interior, opens roof and floor
+#@param ir interior radius (distance from center to wall)
+#@param ht height of the keep relative to sp
+#@param block the substance to build out of
+#@param block_sub number of the block variation
+#@param floors boolean if true, there will be a top and bottom to the keep
+def keep2(ir, ht, gl, block, block_sub, floor, ceiling, filename, xcoor, zcoor):
+    cmds = []
+    i = 1
+    s = '/fill '
+    br = ir+1
+    f= open(filename+".txt", 'a')
+    f.write(s+str(br+xcoor)+' '+str(gl)+' '+str(br+zcoor)+' '+str(xcoor-br)+' '+str(ht+gl)+' '+str(zcoor-br)+' '+block+' '+str(block_sub)+' hollow'+'\n')
+    if floor and ceiling:
+        f.write(s+str(ir+xcoor)+' '+str(gl+1)+' '+str(ir+zcoor)+' '+str(xcoor-ir)+' '+str(ht+gl-1)+' '+str(zcoor-ir)+' '+'air'+'\n')
+    elif floor:
+        f.write(s+str(ir+xcoor)+' '+str(gl+1)+' '+str(ir+zcoor)+' '+str(xcoor-ir)+' '+str(ht+gl)+' '+str(zcoor-ir)+' '+'air'+'\n')
+    else:
+        f.write(s+str(ir+xcoor)+' '+str(gl)+' '+str(ir+zcoor)+' '+str(xcoor-ir)+' '+str(ht+gl-1)+' '+str(zcoor-ir)+' '+'air'+'\n')    
+
     return True
 
 
 #builds keep with no sub_block
 def keep(ir, ht,gl, block, floor, ceiling, filename):
     keep1(ir, ht,gl, block, 0, floor, ceiling, filename)
+    return
+
+def keepAbs(ir, ht,gl, block, floor, ceiling, filename, x, z):
+    keep2(ir, ht,gl, block, 0, floor, ceiling, filename,x,z)
     return
 
 #build a cascading tower that follow phi
@@ -44,6 +67,18 @@ def phiTower(ht, ir,block,gl):
     irr = ir
     for p in philist:
         keep(irr, p,gl,block,False,True,"phiTower"+str(ht)+'_'+str(ir)+'_'+str(gl))
+        irr +=1
+        
+    #END
+
+#build a cascading tower that follow phi
+#@param ht total height
+#@param ir inner radius of uppermost section
+def phiTowerAbs(ht, ir,block,gl,x,y):
+    philist =getPhi2(ht)
+    irr = ir
+    for p in philist:
+        keepAbs(irr, p,gl,block,False,True,"phiTower"+str(ht)+'_'+str(ir)+'_'+str(gl),x,y)
         irr +=1
         
     #END
@@ -99,7 +134,7 @@ def getCurlys(i):
 def test():
     getPhi1(100)
     getPhi2(100)
-    phiTower(150, 3,  "obsidian",61)
+    phiTowerAbs(100,1,"ice",100, 1152,-8)
 
 
 test()
